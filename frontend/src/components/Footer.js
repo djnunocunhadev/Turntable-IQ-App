6904e52bd883463d568a655bf502a7367405ad5e
djnunocunhadev <djnunocunha@gmail.com>
2025-03-11 11:56:50 +0000

    Add Footer component

---
+++ frontend/src/components/Footer.js
@@ -0,0 +1,29 @@
+import React from 'react';
+import { Box, Container, Typography, Link } from '@mui/material';
+
+function Footer() {
+  return (
+    <Box
+      component="footer"
+      sx={{
+        py: 3,
+        px: 2,
+        mt: 'auto',
+        backgroundColor: (theme) => theme.palette.background.paper,
+      }}
+    >
+      <Container maxWidth="lg">
+        <Typography variant="body2" color="text.secondary" align="center">
+          {'© '}
+          {new Date().getFullYear()}{' '}
+          <Link color="inherit" href="/">
+            TurntableIQ
+          </Link>
+          {' - DJ Library Management System'}
+        </Typography>
+      </Container>
+    </Box>
+  );
+}
+
+export default Footer;
